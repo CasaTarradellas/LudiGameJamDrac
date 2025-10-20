570d1a85c621867344de58fe121ba632a5256b48
Enti-Xuezhi <xuezhi.wang@enti.cat>
2025-10-21 00:10:21 +0200

    bug fixed
    Fixed on click trigger multi spawn issue and fix button spawns abnormal size on pc window

--- Drac/Assets/Script/AnswerButton.cs
+++ Drac/Assets/Script/AnswerButton.cs
@@ -1,28 +1,46 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AnswerButton : MonoBehaviour
 {
+    private int answerIndex;
     private GameManager gameManager;
-    public int answerIndex;
 
-    void Start()
+    [SerializeField] private TextMeshProUGUI answerText;
+
+    void Awake()
     {
+        if (answerText == null)
+            answerText = GetComponentInChildren<TextMeshProUGUI>();
+
         gameManager = FindFirstObjectByType<GameManager>();
+
+        if (gameManager == null)
+            Debug.LogError("GameManager not found in the scene!");
     }
 
-    public void SetAnswer(string answerText, int index)
+    public void SetAnswer(string answer, int index)
     {
-        TextMeshProUGUI tmp = GetComponentInChildren<TextMeshProUGUI>();
-        if (tmp != null)
-            tmp.text = answerText;
+        answerIndex = index;
+
+        if (answerText != null)
+            answerText.text = answer;
         else
-            Debug.LogWarning("TextMeshProUGUI component not found in button prefab!");
+            Debug.LogWarning("TextMeshProUGUI component not found on AnswerButton prefab!");
     }
 
-    public void answerChosen()
+    public void anwserChosen()
     {
+        AnswerButton[] allButtons = FindObjectsByType<AnswerButton>(FindObjectsInactive.Exclude, sortMode: FindObjectsSortMode.None);
+
+        foreach (AnswerButton btn in allButtons)
+        {
+            Button uiButton = btn.GetComponent<Button>();
+            if (uiButton != null)
+                uiButton.interactable = false;
+        }
+
         gameManager.userSelectedAnswer(answerIndex);
     }
 }
