c06a148080a2c2514b19f9dcf408f35262cc7cde
CasaTarradellas <guillem.fernandez.lopez@enti.cat>
2025-10-21 02:13:31 +0200

    Muchos Cambios T_T

---
+++ Drac/Assets/Script/Lvl1Button.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lvl1Button : MonoBehaviour
+{
+    public void ChangeLvl1()
+    {
+        SceneManager.LoadScene("Nivell_1");
+    }
+}
