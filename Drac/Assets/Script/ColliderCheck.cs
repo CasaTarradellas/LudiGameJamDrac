0055de393c8a5ab1ff5fbe86cdb29d7f4fcc1592
Guillem Fernandez Lopez <guillem.fernandez.lopez@enti.cat>
2025-10-28 17:07:50 +0100

    Cosas

--- Drac/Assets/Script/ColliderCheck.cs
+++ Drac/Assets/Script/ColliderCheck.cs
@@ -1,14 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class ColliderCheck : MonoBehaviour
 {
+    public ParticleSystem particles;
     void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log(col.gameObject.name);
-        AnswerButton answer = col.GetComponent<AnswerButton>();
+        AnswerButton answer = this.GetComponent<AnswerButton>();
         if (answer != null)
         {
-            answer.AnswerChosen();
+            StartCoroutine(OnCollide(answer));
         }
+    }
+    IEnumerator OnCollide(AnswerButton answer)
+    {
+        this.GetComponent<Collider2D>().enabled = false;
+        this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        this.GetComponent<Animator>().SetTrigger("Collided");
+        particles?.Play();
+        yield return new WaitForSeconds(1.0f);
+        answer.AnswerChosen();
     }
 }
